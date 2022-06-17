19fc1a43486be72efac56deefb4381894c8ae9d6
edgar villamarin <edgarv.uribe@hotmail.com>
2022-06-17 02:16:16 -0500

    validate if dna is rectangular array and early return

--- app/controllers/api/v1/dna_analyzer_controller.rb
+++ app/controllers/api/v1/dna_analyzer_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::DnaAnalyzerController < ApplicationController
+  before_action :validate_params, only: [:mutant]
+
   def mutant
+
+  end
+
+  private
+
+  def dna
+    params.require(:dna)
+  end
+
+  def validate_params
+    return head :forbidden unless valid_dna?(dna)
   end
 end
--- spec/requests/api/v1/dna_analyzer_spec.rb
+++ spec/requests/api/v1/dna_analyzer_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::DnaAnalyzer", type: :request do
-  context "POST /mutant" do
-    it "creates a Widget" do
-      post "/mutant", :params => { :dna => ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"] }
-      expect(response).to have_http_status(:no_content)
+  context "Mutant end-point" do
+
+    describe 'analyze valid' do
+      it 'mutant dna' do
+        post "/mutant", :params => { :dna => ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"] }
+        expect(response).to have_http_status(:no_content)
+      end
+
+      it 'human dna' do
+        post "/mutant", :params => { :dna => ["ATGCGA","CAGTGC","TTATGT","AGAAGG","TGCTTA","TCACTG"] }
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    describe 'analyze bad request' do
+      it 'sent a rectangular array' do
+        post "/mutant", :params => { :dna => ["ATG","CAG"] }
+        expect(response).to have_http_status(:forbidden)
+      end
     end
   end
 end
