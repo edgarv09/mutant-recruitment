82f8871171060f0659572adf51a62432eb694864
edgar villamarin <edgarv.uribe@hotmail.com>
2022-06-17 01:39:57 -0500

    validate if a dna array is valid for us

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
   def valid_dna?(dna)
+    valid_chars?(dna) && valid_size?(dna)
+  end
+
+  def valid_chars?(dna)
     dna.join.match?(/[ACTG]/)
+  end
+
+  def valid_size?(dna)
+    column_size = dna.size
+    total_string_size = dna.join.size
+    (total_string_size / column_size.to_f).floor ==  column_size.to_f
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#valid_dna" do
     let(:valid_dna) { ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"] }
+    let(:invalid_dna) { ["ATGCGA","CHHTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"] }
+
     it "returns true" do
       expect(helper.valid_dna?(valid_dna)).to be_truthy
     end
